669fa2e0fc9eb9945cf15f188d0bd48b45ffab67
mpadge <mark.padgham@email.com>
2024-06-07 16:00:50 +0200

    start prepare-data fn

---
+++ R/prepare-data.R
@@ -0,0 +1,12 @@
+#' Prepare data from an Open Street Map (OSM) file
+#'
+#' @param osm_file A character string giving the path to an OSM file. The
+#' function will work most efficiently if this in in `.pbf` format.
+#' @param osm_boundary_id The OSM identifier of a bounding polygon for the
+#' region of interest.
+#' @param results_dir Directory in which processed OSM extracts are to be saved.
+#' @return A character vector of two elements containing full file paths to data
+#' on the OSM network and natural spaces.
+#' @export
+nr_prepare_data <- function (osm_file, osm_boundary_id, results_dir) {
+}
