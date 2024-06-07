8add5968beee5fb3a175069740e917504d50a5a4
mpadge <mark.padgham@email.com>
2024-06-07 16:40:11 +0200

    add utils.R with uaengine fn reproj_equal_area

---
+++ R/utils.R
@@ -0,0 +1,27 @@
+#' Re-project on to Lambert Azimuthal equal-area projection, centred at center
+#' of actual objects.
+#'
+#' Direct copy of same function from 'uaengine'
+#' @noRd
+reproj_equal_area <- function (x, lon = NULL, lat = NULL) {
+
+    if (is.null (lon) && is.null (lat)) {
+        xy <- sf::st_coordinates (x)
+        xy <- round (apply (xy, 2, mean))
+    } else if (!is.null (lon) && !is.null (lat)) {
+        checkmate::assert_numeric (lon, len = 1L)
+        checkmate::assert_numeric (lat, len = 1L)
+        xy <- round (c (lat, lon))
+    }
+
+    crs <- sf::st_crs (paste0 (
+        "+proj=laea +lat_0=",
+        xy [1],
+        " +lon_0=",
+        xy [2],
+        " +x_0=4321000 +y_0=3210000 +ellps=GRS80 ",
+        "+towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+    ))
+
+    sf::st_transform (x, crs = crs)
+}
